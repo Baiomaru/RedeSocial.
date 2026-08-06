8faa363f02bec9f42ce23c35935d9f53730671f5
Baiomaru <joerick.pereira@escola.pr.gov.br>
2026-08-06 12:14:20 -0300

    ok

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 //botao curtida
 
-document.addEventListener
+document.addEventListener9("DOMCONTENTLoaded",()=>{
+const likeBtn = document querySelector (".let-actions .action-btn:first-child");
+if (!likeBtn) return;
+const likeSvg = likeBtn.querySelector("svg");
+
+
+}
