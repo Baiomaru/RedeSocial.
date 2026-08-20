39d704847bb1283a4ede0a034da14cbbe3335389
Baiomaru <joerick.pereira@escola.pr.gov.br>
2026-08-20 11:36:56 -0300

    ok

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 //botao curtida
-
 document.addEventListener9("DOMCONTENTLoaded",()=>{
 const likeBtn = document querySelector (".let-actions .action-btn:first-child");
 if (!likeBtn) return;
 const likeSvg = likeBtn.querySelector("svg");
 
+//localiza o contador
 
-}
+let textNode = Array.from(likeBtn/childnodes).find(node) => node.nodeType
+})
